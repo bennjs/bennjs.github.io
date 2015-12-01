0f01bf90d15db5332e6c4d7b86fa2c7c08064973
Benjamin Stokes <stokesbnj@gmail.com>
2015-12-01 21:34:57 +0000

    Adds Year One Grades table to Education section.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//Toggles the visibility of my Modules & Grades table
+// .hide can be found on line70 of css/style.css
+var $button = $('#yearOne'),
+  $text   = $('#grades-table'),
+  visible = false;
+
+$button.click(function(){
+if ( visible ) {
+  $text.addClass('hide');
+} else {
+  $text.removeClass('hide');
+}
+visible = ! visible;
+});
+
+// GitHubActivity Stream Widget by Casey Scarborough
+// https://github.com/caseyscarborough/github-activity
 GitHubActivity.feed({
     username: "bennjs",
     selector: "#feed",
